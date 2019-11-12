b0e94c5b099e2b249ad5257e2077e347ebbc53ec
arturx <arturxdroid@gmail.com>
2019-11-12 20:45:51 +0300

    Implemented material cards with adapter

--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardViewHolder.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardViewHolder.kt
@@ -1,7 +1,12 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.toolbox.NetworkImageView
 
-class ProductCardViewHolder(itemView: View) //TODO: Find and store views from itemView
-    : RecyclerView.ViewHolder(itemView)
+class ProductCardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val productImage = itemView.findViewById<NetworkImageView>(R.id.product_image)
+    val productTitle = itemView.findViewById<TextView>(R.id.product_title)
+    val productPrice = itemView.findViewById<TextView>(R.id.product_price)
+}
